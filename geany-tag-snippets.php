2e725ed7451481456cd54aaeb45e6eff90c98c36
Will Hall <will@willhallonline.co.uk>
2015-02-02 13:48:47 +0000

    Updated to separate files and a function - slightly easier to read

--- geany-tag-snippets.php
+++ geany-tag-snippets.php
@@ -3,14 +3,22 @@
  * Maker for translating into snippets
  */
 
-$drupal_tags = file_get_contents('drupal7.php.tags');
-$drupal_tags_lines = explode("\n",$drupal_tags);
-unset($drupal_tags_lines[0],$drupal_tags_lines[1]);
-$totalfile = '';
-foreach ($drupal_tags_lines as $value) 
+function snippet_generator($tag_file, $output_file)
 {
-	$line = explode('|',$value);
-	$totalfile .=  $line[0].'='.$line[0].'('.$line[2].')'.'\n{\n\t%cursor%\n}'."\r\n";
+	$drupal_tags = file_get_contents($tag_file);
+	$drupal_tags_lines = explode("\n",$drupal_tags);
+	unset($drupal_tags_lines[0],$drupal_tags_lines[1]);
+	$file = '';
+	foreach ($drupal_tags_lines as $value) 
+	{
+		//if(strpos($value, '# ') === 0)
+		//{
+			$line = explode('|',$value);
+			$file .=  $line[0].'='.$line[0].'('.$line[2].')'."\r\n";
+		//}
+	}
+	file_put_contents($output_file,$file);
 }
 
-file_put_contents('drupal-snippets.conf',$totalfile);
+snippet_generator('drupal-geany-widget/drupal7.php.tags','drupal7.snippets.conf');
+snippet_generator('drupal-geany-widget/drupal7.views.php.tags','drupal7.views.snippets.conf');
